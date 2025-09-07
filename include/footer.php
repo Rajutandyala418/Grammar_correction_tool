51ffbaeaa9e5de92c3a7f5b74eb45d376a587c0c
TANDYALA RAJU <167282050+Rajutandyala418@users.noreply.github.com>
2025-09-07 22:09:10 +0530

    Delete include directory

--- include/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <div class="footer-content">
-        <p>&copy; <?php echo date("Y"); ?> Bus Booking System. All rights reserved.</p>
-    </div>
-    <script src="/y22cm171/bus_booking/js/main.js"></script>
-</footer>
